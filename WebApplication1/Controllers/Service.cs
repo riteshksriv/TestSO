1d8a0f101be6293651bd1d70d13e809d6d1346ad
Ritesh Kumar (from Dev Box) <riteshkumar@microsoft.com>
2025-02-15 19:46:52 +0530

    init

---
+++ WebApplication1/Controllers/Service.cs
@@ -0,0 +1,520 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class Service
+    {
+        public Service() { }
+
+        public async Task Method1()
+        {
+            await Method2();
+        }
+        public async Task Method100()
+        {
+            Service2 service2 = new Service2();
+            await service2.Method1();
+            String str = "Hello World";
+            Console.WriteLine(str.Substring(1));
+            await Task.Delay(1000);
+        }
+
+        public async Task Method2()
+        {
+            await Method3();
+        }
+
+
+
+        public async Task Method3()
+        {
+            await Method4();
+        }
+
+        public async Task Method4()
+        {
+            await Method5();
+        }
+
+        public async Task Method5()
+        {
+            await Method6();
+        }
+
+        public async Task Method6()
+        {
+            await Method7();
+        }
+
+        public async Task Method7()
+        {
+            await Method8();
+        }
+
+        public async Task Method8()
+        {
+            await Method9();
+        }
+
+        public async Task Method9()
+        {
+            await Method10();
+        }
+
+        public async Task Method10()
+        {
+            await Method11();
+        }
+
+        public async Task Method11()
+        {
+            await Method12();
+        }
+
+        public async Task Method12()
+        {
+            await Method13();
+        }
+
+        public async Task Method13()
+        {
+            await Method14();
+        }
+
+        public async Task Method14()
+        {
+            await Method15();
+        }
+
+        public async Task Method15()
+        {
+            await Method16();
+        }
+
+        public async Task Method16()
+        {
+            await Method17();
+        }
+
+        public async Task Method17()
+        {
+            await Method18();
+        }
+
+        public async Task Method18()
+        {
+            await Method19();
+        }
+
+        public async Task Method19()
+        {
+            await Method20();
+        }
+
+        public async Task Method20()
+        {
+            await Method21();
+        }
+
+        public async Task Method21()
+        {
+            await Method22();
+        }
+
+        public async Task Method22()
+        {
+            await Method23();
+        }
+
+        public async Task Method23()
+        {
+            await Method24();
+        }
+
+        public async Task Method24()
+        {
+            await Method25();
+        }
+
+        public async Task Method25()
+        {
+            await Method26();
+        }
+
+        public async Task Method26()
+        {
+            await Method27();
+        }
+
+        public async Task Method27()
+        {
+            await Method28();
+        }
+
+        public async Task Method28()
+        {
+            await Method29();
+        }
+
+        public async Task Method29()
+        {
+            await Method30();
+        }
+
+        public async Task Method30()
+        {
+            await Method31();
+        }
+
+        public async Task Method31()
+        {
+            await Method32();
+        }
+
+        public async Task Method32()
+        {
+            await Method33();
+        }
+
+        public async Task Method33()
+        {
+            await Method34();
+        }
+
+        public async Task Method34()
+        {
+            await Method35();
+        }
+
+        public async Task Method35()
+        {
+            await Method36();
+        }
+
+        public async Task Method36()
+        {
+            await Method37();
+        }
+
+        public async Task Method37()
+        {
+            await Method38();
+        }
+
+        public async Task Method38()
+        {
+            await Method39();
+        }
+
+        public async Task Method39()
+        {
+            await Method40();
+        }
+
+        public async Task Method40()
+        {
+            await Method41();
+        }
+
+        public async Task Method41()
+        {
+            await Method42();
+        }
+
+        public async Task Method42()
+        {
+            await Method43();
+        }
+
+        public async Task Method43()
+        {
+            await Method44();
+        }
+
+        public async Task Method44()
+        {
+            await Method45();
+        }
+
+        public async Task Method45()
+        {
+            await Method46();
+        }
+
+        public async Task Method46()
+        {
+            await Method47();
+        }
+
+        public async Task Method47()
+        {
+            await Method48();
+        }
+
+        public async Task Method48()
+        {
+            await Method49();
+        }
+
+        public async Task Method49()
+        {
+            await Method50();
+        }
+
+        public async Task Method50()
+        {
+            await Method51();
+        }
+
+        public async Task Method51()
+        {
+            await Method52();
+        }
+
+        public async Task Method52()
+        {
+            await Method53();
+        }
+
+        public async Task Method53()
+        {
+            await Method54();
+        }
+
+        public async Task Method54()
+        {
+            await Method55();
+        }
+
+        public async Task Method55()
+        {
+            await Method56();
+        }
+
+        public async Task Method56()
+        {
+            await Method57();
+        }
+
+        public async Task Method57()
+        {
+            await Method58();
+        }
+
+        public async Task Method58()
+        {
+            await Method59();
+        }
+
+        public async Task Method59()
+        {
+            await Method60();
+        }
+
+        public async Task Method60()
+        {
+            await Method61();
+        }
+
+        public async Task Method61()
+        {
+            await Method62();
+        }
+
+        public async Task Method62()
+        {
+            await Method63();
+        }
+
+        public async Task Method63()
+        {
+            await Method64();
+        }
+
+        public async Task Method64()
+        {
+            await Method65();
+        }
+
+        public async Task Method65()
+        {
+            await Method66();
+        }
+
+        public async Task Method66()
+        {
+            await Method67();
+        }
+
+        public async Task Method67()
+        {
+            await Method68();
+        }
+
+        public async Task Method68()
+        {
+            await Method69();
+        }
+
+        public async Task Method69()
+        {
+            await Method70();
+        }
+
+        public async Task Method70()
+        {
+            await Method71();
+        }
+
+        public async Task Method71()
+        {
+            await Method72();
+        }
+
+        public async Task Method72()
+        {
+            await Method73();
+        }
+
+        public async Task Method73()
+        {
+            await Method74();
+        }
+
+        public async Task Method74()
+        {
+            await Method75();
+        }
+
+        public async Task Method75()
+        {
+            await Method76();
+        }
+
+        public async Task Method76()
+        {
+            await Method77();
+        }
+
+        public async Task Method77()
+        {
+            await Method78();
+        }
+
+        public async Task Method78()
+        {
+            await Method79();
+        }
+
+        public async Task Method79()
+        {
+            await Method80();
+        }
+
+        public async Task Method80()
+        {
+            await Method81();
+        }
+
+        public async Task Method81()
+        {
+            await Method82();
+        }
+
+        public async Task Method82()
+        {
+            await Method83();
+        }
+
+        public async Task Method83()
+        {
+            await Method84();
+        }
+
+        public async Task Method84()
+        {
+            await Method85();
+        }
+
+        public async Task Method85()
+        {
+            await Method86();
+        }
+
+        public async Task Method86()
+        {
+            await Method87();
+        }
+
+        public async Task Method87()
+        {
+            await Method88();
+        }
+
+        public async Task Method88()
+        {
+            await Method89();
+        }
+
+        public async Task Method89()
+        {
+            await Method90();
+        }
+
+        public async Task Method90()
+        {
+            await Method91();
+        }
+
+        public async Task Method91()
+        {
+            await Method92();
+        }
+
+        public async Task Method92()
+        {
+            await Method93();
+        }
+
+        public async Task Method93()
+        {
+            await Method94();
+        }
+
+        public async Task Method94()
+        {
+            await Method95();
+        }
+
+        public async Task Method95()
+        {
+            await Method96();
+        }
+
+        public async Task Method96()
+        {
+            await Method97();
+        }
+
+        public async Task Method97()
+        {
+            await Method98();
+        }
+
+        public async Task Method98()
+        {
+            await Method99();
+        }
+
+        public async Task Method99()
+        {
+            await Method100();
+        }
+
+
+    }
+}
---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public async Task<string> Get()
+        {
+            Service2 service2 = new Service2();
+            await service2.Method1().ConfigureAwait(false);
+            Service service = new Service();
+            await service.Method1().ConfigureAwait(false);
+            return await Task.FromResult("value");
+        }
+
+        async Task NoOp()
+        {
+            await Task.Delay(1000);
+        }
+
+        async Task AnotherMethod()
+        {
+            Request.Properties["test1"] = "Name";
+            await Task.Delay(1000).ConfigureAwait(false);
+            await MethodThrowsException().ConfigureAwait(false);
+            Request.Properties["test"] = "Name";
+        }
+
+        async Task<string> MethodThrowsException()
+        {
+            await Task.Delay(1000).ConfigureAwait(false);
+            throw new ArgumentNullException("Exception thrown from async method");
+        }
+    }
+}
